39487ef916c09f802536b33aa899f269f0fcc3af
Jaxon Carelos <booga956@gmail.com>
2023-07-08 17:15:57 -0600

    Fix

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
 def is_valid_url(url):
     for i in regex:
         if re.match(regex[i], url):
@@ -28,6 +27,7 @@ def is_valid_url(url):
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.invisible, activity=discord.Game(name="ffmpreg"))
     print(f'{client.user} has connected to Discord!')
 
 @client.event
@@ -36,30 +36,30 @@ async def on_message(message):
     if message.author == client.user:
         return
     is_valid = is_valid_url(content)
-    if is_valid:
-        should_download = False
-        print(content, is_valid)
-        match is_valid:
-            case "twitter":
-                output = subprocess.run(["yt-dlp", "-g", '-f', 'best', '--no-warnings', content], capture_output=True)
-                await message.reply(output.stdout.decode('utf-8'))
-            case "tiktok":
-                should_download = True
-            case "reddit":
-                should_download = True
-        if should_download:
-            if(os.path.isfile('output.mp4')):
-                os.remove('output.mp4')            
-            subprocess.run(["yt-dlp",                                   
-                            "-f", "bestvideo[filesize<6MB]+bestaudio[filesize<2MB]/best/bestvideo+bestaudio",
-                            "-S", "vcodec:h264",
-                            "--merge-output-format", "mp4",
-                            "--ignore-config",
-                            "--no-playlist",
-                            "--no-warnings", '-o', 'output.mp4', content])
-            with open('output.mp4', 'rb') as file:
-                await message.reply(mention_author=False, file=discord.File(file, 'output.mp4'))
-            os.remove('output.mp4')
+    if not is_valid:
+        return    
+    should_download = False
+    match is_valid:
+        case "twitter":
+            output = subprocess.run(["yt-dlp", "-g", '-f', 'best', '--no-warnings', content], capture_output=True)
+            await message.reply(output.stdout.decode('utf-8'))
+        case "tiktok":
+            should_download = True
+        case "reddit":
+            should_download = True
+    if should_download:
+        if(os.path.isfile('output.mp4')):
+            os.remove('output.mp4')            
+        subprocess.run(["yt-dlp",                                   
+                        "-f", "bestvideo[filesize<6MB]+bestaudio[filesize<2MB]/best/bestvideo+bestaudio",
+                        "-S", "vcodec:h264",
+                        "--merge-output-format", "mp4",
+                        "--ignore-config",
+                        "--no-playlist",
+                        "--no-warnings", '-o', 'output.mp4', content])
+        with open('output.mp4', 'rb') as file:
+            await message.reply(mention_author=False, file=discord.File(file, 'output.mp4'))
+        os.remove('output.mp4')
 
             
 
